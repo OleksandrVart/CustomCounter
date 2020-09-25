d9d8e64ed17e7dbb00f79061123f19615059bfe7
Regin <Regin@VINGDEMON>
2020-09-25 12:41:50 +0300

    Добавьте файлы проекта.

---
+++ CustomCounter/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CustomCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.counterStep = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(57, 245);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(141, 56);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStop.Location = new System.Drawing.Point(229, 245);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(141, 56);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Стоп";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // counterStep
+            // 
+            this.counterStep.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.counterStep.Location = new System.Drawing.Point(229, 67);
+            this.counterStep.Name = "counterStep";
+            this.counterStep.Size = new System.Drawing.Size(61, 32);
+            this.counterStep.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(132, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Шаг:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(52, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Текущее значение:";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(303, 149);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(26, 29);
+            this.result.TabIndex = 5;
+            this.result.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 351);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.counterStep);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Custom counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox counterStep;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ CustomCounter/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomCounter
+{
+    public partial class Form1 : Form
+    {
+        private delegate void SafeCallDelegate(string text);
+        System.Threading.Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            TimerCallback tm = new TimerCallback(Count);
+            timer = new System.Threading.Timer(tm, null, 0, 1000);
+        }
+
+        private void Count(object obj)
+        {
+            int nextStep = 1;
+            int step;
+
+            try
+            {
+                step = int.Parse(counterStep.Text);
+                if (step != 0)
+                {
+                    nextStep = step;
+                }
+            }
+            catch {}
+            finally {}
+            
+            
+            int calcCount = int.Parse(result.Text);
+            calcCount += nextStep;
+            WriteTextSafe(calcCount.ToString());
+            logging(calcCount.ToString());
+        }
+
+        private void WriteTextSafe(string text)
+        {
+            if (result.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(WriteTextSafe);
+                result.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                result.Text = text;
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer.Change(Timeout.Infinite, 0);
+        }
+
+        private void logging(string text)
+        {
+            string filename = @"\counter_logs.txt";
+            string path = Directory.GetCurrentDirectory() + filename;
+            DateTime curDate = DateTime.Now;
+
+            FileStream file = new FileStream(path, FileMode.Append);
+            StreamWriter fnew = new StreamWriter(file, Encoding.GetEncoding(1251));
+
+            fnew.WriteLine(curDate.ToLocalTime() + " - " + text);
+            fnew.Close();
+        }
+    }
+}
